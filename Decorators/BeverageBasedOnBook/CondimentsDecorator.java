2224baa464bf531baa8672b5abada244605a376a
Emman-pip <parrenoemmanuel756@gmail.com>
2025-01-03 09:33:59 +0800

    Add condiments decorator class.
    A subclass of the Beverage class.

---
+++ Decorators/BeverageBasedOnBook/CondimentsDecorator.java
@@ -0,0 +1,10 @@
+/*
+ * CondimentsDecorator
+ * A subclass of the Beverage class
+ *
+ * This is where the condiments will inherit from
+ */
+
+public abstract class CondimentsDecorator extends Beverage {
+	public abstract String getDescription();
+}
